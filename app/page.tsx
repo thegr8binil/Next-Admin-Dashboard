e5e5cf9395fdad65a80258f7440e51f5f29a6649
Binil B <binil.b.re@gmail.com>
2024-05-27 18:28:59 +0530

    dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -32,6 +32,7 @@ import {
 import { Input } from "@/components/ui/input"
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 import { ModeToggle } from "@/components/ui/ModeToggle"
+import Orders from "@/components/features/orders"
 
 export default function Dashboard() {
   return (
@@ -221,19 +222,11 @@ export default function Dashboard() {
           <div className="flex items-center">
             <h1 className="text-lg font-semibold md:text-2xl">Inventory</h1>
           </div>
-          <div
-            className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm" x-chunk="dashboard-02-chunk-1"
-          >
-            <div className="flex flex-col items-center gap-1 text-center">
-              <h3 className="text-2xl font-bold tracking-tight">
-                You have no products
-              </h3>
-              <p className="text-sm text-muted-foreground">
-                You can start selling as soon as you add a product.
-              </p>
-              <Button className="mt-4">Add Product</Button>
-            </div>
-          </div>
+         
+            
+             <Orders/>
+           
+         
         </main>
       </div>
     </div>
